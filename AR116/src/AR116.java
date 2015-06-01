7afe3d5d5263136e87c92b3ef331efc62432a6f1
Peter <pdurandi@gmail.com>
2015-06-01 18:56:32 +0200

    AC // Tiempo: 0.02 // Memoria: 83 // Pos: 261

--- AR116/src/AR116.java
+++ AR116/src/AR116.java
@@ -1,17 +1,20 @@
+import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 
 /**
  * @author Peter
  * @see https://www.aceptaelreto.com/problem/statement.php?id=116
- * @result AC // Tiempo: 0.024 // Memoria: 83 // Pos: 478
+ * @result AC // Tiempo: 0.02 // Memoria: 83 // Pos: 261
  */
 
 public class AR116 {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		OutputStream out = new BufferedOutputStream(System.out);
 		byte n = new Byte(br.readLine());
-		for (byte i=0; i<n; i++) System.out.println("Hola mundo.");
+		for (byte i=0; i<n; i++) { out.write(("Hola mundo.\n").getBytes()); out.flush(); }
 	}
 }
