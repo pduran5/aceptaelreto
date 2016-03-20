17a6b32b6d14147e5bfd2e40da08a560f2e0fd4e
Peter <Peter@192.168.1.131>
2016-03-20 12:48:42 +0100

    @author EllaNoSeDeJava (CEROCA)

---
+++ aceptaelreto/src/AR335v2.java
@@ -0,0 +1,28 @@
+/**
+ * @author EllaNoSeDeJava (CEROCA)
+ * @see https://www.aceptaelreto.com/problem/statement.php?id=335
+ * @result 
+ */
+
+import java.util.Scanner;
+
+public class AR335v2 {
+    public static void main(String[] args) {
+        Scanner lector = new Scanner(System.in);
+        int casos = lector.nextInt();
+        
+        for (int i = 0; i < casos; i++) {
+            long filas_piramide = lector.nextLong();
+            long canicas_2d=0;
+            long canicas_3d=0;
+            
+            for(int j = 1; j <= filas_piramide; j++) {
+                canicas_2d += j;
+                canicas_3d += canicas_2d;
+            }
+            
+            System.out.println(canicas_3d);
+        }     
+    }
+}
+
